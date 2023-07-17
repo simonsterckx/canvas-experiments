8be8608c32eb18baac4ff8c05837a6e02a8369e9
Simon Sterckx <se.sterckx@gmail.com>
2023-07-17 14:38:40 +0200

    move joystick

--- src/VirtualJoystick.ts
+++ src/VirtualJoystick.ts
@@ -1,40 +1,32 @@
+import { isMobile } from "./Constants";
+
+const JOYSTICK_WIDTH = 200;
+const HALF_JOYSTICK_WIDTH = JOYSTICK_WIDTH / 2;
+const JOYSTICK_HEIGHT = 200;
+const HALF_JOYSTICK_HEIGHT = JOYSTICK_HEIGHT / 2;
+const JOYSTICK_RADIUS = 50;
+const JOYSTICK_MAX_RADIUS = 50;
+const TWO_PI = Math.PI * 2;
 export class VirtualJoystick {
-  x: number;
-  y: number;
-  radius: number;
-  maxRadius: number;
-  isPressed: boolean;
-  isMobile: boolean;
+  x: number = 0;
+  y: number = 0;
+  radius: number = 0;
+  maxRadius: number = 0;
+  isPressed = false;
+  isMobile = false;
   canvas!: HTMLCanvasElement;
-  ctx: any;
+  ctx!: CanvasRenderingContext2D;
   constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.radius = 0;
-    this.maxRadius = 0;
     this.isPressed = false;
-    this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (!this.isMobile) return;
+    if (!isMobile) return;
 
     this.canvas = document.createElement("canvas");
-    this.ctx = this.canvas.getContext("2d");
-    this.canvas.width = 200;
-    this.canvas.height = 200;
-    this.radius = 50;
-    this.maxRadius = 50;
-    this.canvas.style.position = "absolute";
-    // Bottom right
-    this.canvas.style.bottom = "20px";
-    this.canvas.style.right = "20px";
-    this.canvas.style.transform = "translate(-50%, -50%)";
-    this.canvas.style.touchAction = "none";
-    this.canvas.style.userSelect = "none";
-    this.canvas.style.borderRadius = "50%";
-    this.canvas.style.background = "rgba(0, 0, 0, 0.1)";
-    this.canvas.style.boxShadow = "0 0 0 1px rgba(0, 0, 0, 0.1)";
-    this.canvas.style.cursor = "pointer";
-    this.canvas.style.touchAction = "none";
+    this.ctx = this.canvas.getContext("2d")!;
+    this.canvas.width = JOYSTICK_WIDTH;
+    this.canvas.height = JOYSTICK_HEIGHT;
+    this.canvas.className = "joystick";
     document.body.appendChild(this.canvas);
+
     this.canvas.addEventListener("mousedown", this.onMouseDown);
     this.canvas.addEventListener("touchstart", this.onMouseDown);
     this.canvas.addEventListener("mouseup", this.onMouseUp);
@@ -60,8 +52,10 @@ export class VirtualJoystick {
       const rect = this.canvas.getBoundingClientRect();
       const x = e instanceof MouseEvent ? e.clientX : e.touches[0].clientX;
       const y = e instanceof MouseEvent ? e.clientY : e.touches[0].clientY;
-      this.x = (x - rect.left - this.canvas.width / 2) / this.maxRadius;
-      this.y = (y - rect.top - this.canvas.height / 2) / this.maxRadius;
+
+      this.x = (x - rect.left - HALF_JOYSTICK_WIDTH) / JOYSTICK_MAX_RADIUS;
+      this.y = (y - rect.top - HALF_JOYSTICK_HEIGHT) / JOYSTICK_MAX_RADIUS;
+
       if (this.x > 1) this.x = 1;
       if (this.x < -1) this.x = -1;
       if (this.y > 1) this.y = 1;
@@ -71,14 +65,14 @@ export class VirtualJoystick {
     }
   };
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, JOYSTICK_WIDTH, JOYSTICK_HEIGHT);
     this.ctx.beginPath();
     this.ctx.arc(
-      this.canvas.width / 2,
-      this.canvas.height / 2,
-      this.radius,
+      HALF_JOYSTICK_WIDTH,
+      HALF_JOYSTICK_HEIGHT,
+      JOYSTICK_RADIUS,
       0,
-      2 * Math.PI
+      TWO_PI
     );
     this.ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
     this.ctx.fill();
@@ -86,11 +80,11 @@ export class VirtualJoystick {
     if (this.isPressed) {
       this.ctx.beginPath();
       this.ctx.arc(
-        this.canvas.width / 2 + this.x * this.maxRadius,
-        this.canvas.height / 2 + this.y * this.maxRadius,
-        this.radius / 2,
+        HALF_JOYSTICK_WIDTH + this.x * JOYSTICK_MAX_RADIUS,
+        HALF_JOYSTICK_HEIGHT + this.y * JOYSTICK_MAX_RADIUS,
+        JOYSTICK_RADIUS / 2,
         0,
-        2 * Math.PI
+        TWO_PI
       );
       this.ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
       this.ctx.fill();
